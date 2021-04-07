00e5b4a8acce84196fc2e4644bac499c0741688e
FAKRUL-77 <fakrul15-1316@diu.edu.bd>
2021-04-07 14:16:26 +0600

    fixing error

--- UI_DESIGN/Responsive-UI/first_demo_r_ui/lib/widgets/navigationBar/navigation_bar.dart
+++ UI_DESIGN/Responsive-UI/first_demo_r_ui/lib/widgets/navigationBar/navigation_bar.dart
@@ -3,6 +3,39 @@ import 'package:flutter/material.dart';
 class NavigationBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: 100,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SizedBox(
+            height: 80,
+            width: 150,
+            child: Image.asset('assets/images/navBarLogo.png'),
+          ),
+          Row(
+            children: [
+              _NavBarItem('Episodes'),
+              SizedBox(
+                width: 60,
+              ),
+              _NavBarItem('About'),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _NavBarItem extends StatelessWidget {
+  final String title;
+  const _NavBarItem(this.title);
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: TextStyle(fontSize: 18),
+    );
   }
 }
