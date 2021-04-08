6c83e4d670eed12b30721b03e47590b38c1c7639
FAKRUL-77 <fakrul15-1316@diu.edu.bd>
2021-04-08 22:23:26 +0600

    ADDED: Animation Builder added

--- Animation/animation/lib/widget/heart.dart
+++ Animation/animation/lib/widget/heart.dart
@@ -6,39 +6,42 @@ class Heart extends StatefulWidget {
 }
 
 class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
-
   late AnimationController _controller;
   late Animation _colorAnimation;
 
   @override
   void initState() {
     _controller = AnimationController(
-      duration: Duration(milliseconds: 200),
-      vsync: this
+      duration: Duration(milliseconds: 300),
+      vsync: this,
     );
     super.initState();
 
-    _colorAnimation = ColorTween(begin: Colors.grey[400], end: Colors.red).animate(_controller);
+    _colorAnimation = ColorTween(begin: Colors.grey[400], end: Colors.red)
+        .animate(_controller);
 
-    _controller.forward();
     _controller.addListener(() {
       print(_controller.value);
       print(_colorAnimation.value);
     });
   }
 
-
-
-
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(
-        Icons.favorite,
-        color: Colors.grey[400],
-        size: 30,
-      ),
-      onPressed: () {},
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (BuildContext context, _) {
+        return IconButton(
+          icon: Icon(
+            Icons.favorite,
+            color: _colorAnimation.value,
+            size: 30,
+          ),
+          onPressed: () {
+            _controller.forward();
+          },
+        );
+      },
     );
   }
 }
